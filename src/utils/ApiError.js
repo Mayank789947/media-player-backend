f3f4fceb1a33aefb50b7d0d43b0ad99ac5a1fbe5
Mayank Rawat <rawatmayankofc25@gmail.com>
2025-04-05 10:08:52 +0530

    add register controller and some bug fixes

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,4 +1,4 @@
-class APIError extends  Error {
+class ApiError extends Error {
     constructor( 
         statusCode,
         message = "Something went wrong",
@@ -22,4 +22,4 @@ class APIError extends  Error {
 }
 
 
-export {APIError}
+export {ApiError}
